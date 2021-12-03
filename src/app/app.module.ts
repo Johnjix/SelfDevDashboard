f70433642862992f5f6ddc0f39e09f9b692fe6b7
John Jix <John_jix@hotmail.co.uk>
2021-12-03 23:13:33 +0000

    Added main dashboard scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { DashboardCommunityComponent } from './dashboard-community/dashboard-community.component';
+import { DashboardUserComponent } from './dashboard-user/dashboard-user.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, BrowserAnimationsModule],
+  declarations: [AppComponent, DashboardComponent, DashboardCommunityComponent, DashboardUserComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
